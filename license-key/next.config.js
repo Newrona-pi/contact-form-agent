49efa0d9ff140028a5e55cd9cba9cfb649d9f2ba
Newrona-pi <pineseigyousyo@gmail.com>
2025-09-12 11:22:34 +0900

    fix: externalize all argon2 variants

--- license-key/next.config.js
+++ license-key/next.config.js
@@ -3,10 +3,15 @@ import path from 'path'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Remove deprecated appDir setting
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(process.cwd(), 'src'),
+    }
+    if (isServer) {
+      config.externals = config.externals || []
+      // Exclude all @node-rs/argon2 variants so Next.js loads them at runtime
+      config.externals.push(/^@node-rs\/argon2.*$/)
     }
     return config
   },
